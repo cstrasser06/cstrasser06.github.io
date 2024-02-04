6c4ea6e1a5621ded0028449341143696b625e150
cstrasser06 <144207619+cstrasser06@users.noreply.github.com>
2024-02-04 21:14:39 +0100

    Update java.js

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 function myFunction(){
     var val = document.getElementById('searchvalue').value;
     var text = val + ".html";
-    window.open("https://cstrasser06.github.io/", text);
+    window.open(text);
 }
