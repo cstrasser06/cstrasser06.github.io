cb1b8d114b42e3b89756e17dd779977b0693f40e
Carmen Strasser <c.strasser@htlkrems.at>
2024-03-24 16:30:42 +0100

    difference ios, android

--- java.js
+++ java.js
@@ -1,9 +1,12 @@
-var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
-var isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+document.addEventListener("DOMContentLoaded", function(){
+    var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
+    var isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-if (isAndroid) {
-    document.body.style.backgroundColor = "red";
-}
-if (isiOS) {
-    document.body.style.backgroundColor = "yellow";
-}
+    if (isAndroid) {
+        // Fügen Sie Ihre Android-spezifischen CSS-Klassen hinzu
+    }
+    if (isiOS) {
+        // Fügen Sie Ihre iOS-spezifischen CSS-Klassen hinzu
+        document.body.style.backgroundColor = "yellow";
+    }
+});
